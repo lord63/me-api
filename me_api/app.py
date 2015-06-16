5dd35cbc04283766ae1508711221c7be5a28ec88
lord63 <lord63.j@gmail.com>
2015-06-16 10:49:12 +0800

    Automatically load the specified blueprint

--- me_api/app.py
+++ me_api/app.py
@@ -6,17 +6,21 @@ from __future__ import absolute_import, unicode_literals
 
 from flask import Flask
 
+from .middleware.me import me
+from .middleware import github, medium
+
 
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(config)
-
-    from .middleware.me import me
-    from .middleware.github import github_api
-    from .middleware.medium import medium_api
+    modules = config.modules['modules']
+    blueprints = {
+        'github': github.github_api,
+        'medium': medium.medium_api
+    }
 
     app.register_blueprint(me)
-    app.register_blueprint(github_api)
-    app.register_blueprint(medium_api)
+    for module in modules.keys():
+        app.register_blueprint(blueprints[module])
 
     return app
