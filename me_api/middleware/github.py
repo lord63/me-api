a80f9e501502fc6069c3186a869695e16136669e
lord63 <lord63.j@gmail.com>
2015-06-17 09:08:07 +0800

    Handle exception for github

--- me_api/middleware/github.py
+++ me_api/middleware/github.py
@@ -16,6 +16,13 @@ github_api = Blueprint('github', __name__)
 
 @github_api.route(path)
 def github():
-    response = requests.get(
-        'https://api.github.com/users/{0}/events/public'.format(username))
-    return jsonify(github=response.json())
+    # TODO: how to handle exception and error, see issue#1.
+    try:
+        response = requests.get(
+            'https://api.github.com/users/{0}/events/public'.format(username))
+    except requests.RequestException as error:
+        return jsonify(error_message=str(error.message))
+    if response.status_code == 200:
+        return jsonify(github=response.json())
+    else:
+        return jsonify(status_code=response.status_code)
