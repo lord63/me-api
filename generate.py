34839421c0fb22e670f97e49630e96e47e21011f
lord63 <lord63.j@gmail.com>
2015-06-18 10:47:44 +0800

    Make template global

--- generate.py
+++ generate.py
@@ -9,16 +9,32 @@ from os import path
 from collections import defaultdict
 
 
-PATH = path.join(path.dirname(path.abspath(__file__)), 'me_api')
 if sys.version_info.major == 2:
     get_input = raw_input
 else:
     get_input = input
+# Json files should be under me_api folder, so get the abspath.
+PATH = path.join(path.dirname(path.abspath(__file__)), 'me_api')
+# Template file for modules.
+TEMPLATE = {
+    "github": {
+        "path": "Input the path(e.g. /github): ",
+        "data": {
+            "me": "Input the username(e.g. lord63): "
+        }
+    },
+    "medium": {
+        "path": "Input the path(e.g. /medium): ",
+        "data": {
+            "me": "Input the username(e.g. @lord63): "
+        }
+    }
+}
 
 
-def config_module(module, template):
+def config_module(module):
     """Config module from user input."""
-    default_conf = template[module]
+    default_conf = TEMPLATE[module]
     for key, value in default_conf.items():
         if isinstance(value, dict):
             for inner_key, inner_value in value.items():
@@ -30,22 +46,6 @@ def config_module(module, template):
 
 def init():
     """Initialize the me.json and modules.json."""
-    # Template file for modules.
-    template = {
-        "github": {
-            "path": "Input the path(e.g. /github): ",
-            "data": {
-                "me": "Input the username(e.g. lord63): "
-            }
-        },
-        "medium": {
-            "path": "Input the path(e.g. /medium): ",
-            "data": {
-                "me": "Input the username(e.g. @lord63): "
-            }
-        }
-    }
-
     print("Initialize me.json.")
     with open(path.join(PATH, 'me.json'), 'w') as f:
         me = {'name': get_input("Input your name: ")}
@@ -56,13 +56,13 @@ def init():
     chosen_modules = get_input(
         "Please choose modules from the following:\n"
         "\t{0}.\n"
-        "Seperate then with a space: ".format(', '.join(template.keys()))
+        "Seperate then with a space: ".format(', '.join(TEMPLATE.keys()))
     ).split()
     with open(path.join(PATH, 'modules.json'), 'w') as f:
         modules = defaultdict(dict)
         for module in chosen_modules:
             print("\nConfig {0} module...".format(module))
-            modules['modules'][module] = config_module(module, template)
+            modules['modules'][module] = config_module(module)
         json.dump(modules, f, indent=4)
     print('\nDone: generate modules.json.')
 
