6a62351ba1fc814c1a5f841ae58d8c333ef0f287
lord63 <lord63.j@gmail.com>
2015-06-21 10:19:52 +0800

    We have a very basic test now

---
+++ tests/test_modules.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, unicode_literals
+
+import unittest
+import json
+
+from me_api.app import create_app
+from me_api.configs import TestingConfig
+
+
+class MeAPITestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app(TestingConfig).test_client()
+
+    def test_index(self):
+        rv = self.app.get('/')
+        assert 'me' in json.loads(rv.data).keys()
+        assert 'routers' in json.loads(rv.data).keys()
