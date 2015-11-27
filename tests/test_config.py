0cd0763a0dfaeb564866680fd2314223c9f0a635
lord63 <lord63.j@gmail.com>
2015-11-27 09:01:44 +0800

    Test cache type

--- tests/test_config.py
+++ tests/test_config.py
@@ -10,13 +10,16 @@ from me_api.configs import DevelopConfig, ProductionConfig, TestingConfig
 def test_develop_config():
     app = create_app(DevelopConfig)
     assert app.config['DEBUG'] is True
+    assert app.config['CACHE_TYPE'] == 'null'
 
 
 def test_production_config():
     app = create_app(ProductionConfig)
     assert app.config['DEBUG'] is False
+    assert app.config['CACHE_TYPE'] == 'redis'
 
 
 def test_testing_config():
     app = create_app(TestingConfig)
     assert app.config['TESTING'] is True
+    assert app.config['CACHE_TYPE'] == 'null'
