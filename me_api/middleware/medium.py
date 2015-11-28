494abe4e122e6242356d8fbf1aebd8ba3c17a454
lord63 <lord63.j@gmail.com>
2015-11-28 15:56:03 +0800

    Simplify loading config for medium

--- me_api/middleware/medium.py
+++ me_api/middleware/medium.py
@@ -6,13 +6,12 @@ from __future__ import absolute_import
 import requests
 from flask import Blueprint, jsonify, json
 
-from me_api.configs import Config
 from me_api.cache import cache
+from me_api.middleware.utils import MiddlewareConfig
 
 
-config = Config.modules['modules']['medium']
-path, username = config['path'], config['data']['me']
-medium_api = Blueprint('medium', __name__, url_prefix=path)
+config = MiddlewareConfig('medium')
+medium_api = Blueprint('medium', __name__, url_prefix=config.path)
 
 
 @medium_api.route('/')
@@ -20,7 +19,7 @@ medium_api = Blueprint('medium', __name__, url_prefix=path)
 def medium():
     try:
         response = requests.get(
-            'https://medium.com/{0}?format=json'.format(username))
+            'https://medium.com/{0}?format=json'.format(config.username))
     except requests.RequestException as error:
         return jsonify(error_message=str(error.message))
     if response.status_code == 200:
