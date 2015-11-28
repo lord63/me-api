2d846a061ee1611e8a8b19d31437c02401d7d1a5
lord63 <lord63.j@gmail.com>
2015-11-28 15:47:23 +0800

    Simplify loading config for douban middleware

--- me_api/middleware/douban.py
+++ me_api/middleware/douban.py
@@ -7,40 +7,34 @@ import os
 import requests
 from flask import Blueprint, jsonify, request, redirect
 
-from me_api.configs import Config
 from me_api.cache import cache
+from me_api.middleware.utils import MiddlewareConfig
 
 
-config = Config.modules['modules']['douban']
-path = config['path']
-me, client_secret, access_token, client_id = (
-    config['data']['me'],
-    config['data']['client_secret'],
-    config['data']['access_token'],
-    config['data']['client_id']
-)
-douban_api = Blueprint('douban', __name__, url_prefix=path)
+config = MiddlewareConfig('douban')
+douban_api = Blueprint('douban', __name__, url_prefix=config.path)
 
 
 @douban_api.route('/')
 @cache.cached(timeout=3600)
 def douban():
-    if not access_token:
+    if not config.access_token:
         return 'Need access token, please authenticate your app first.'
     response = requests.get(
-        "https://api.douban.com/shuo/v2/statuses/user_timeline/{0}".format(me),
-        headers={'Authorization': 'Bearer ' + access_token})
+        ("https://api.douban.com/shuo/v2/statuses/"
+         "user_timeline/{0}").format(config.me),
+        headers={'Authorization': 'Bearer ' + config.access_token})
     return jsonify(douban=response.json())
 
 
 @douban_api.route('/login')
 def authorization():
-    if access_token:
+    if config.access_token:
         return "You've already had an access token in the config file."
     authorization_url = 'https://www.douban.com/service/auth2/auth'
     return redirect(
         '{0}?client_id={1}&redirect_uri={2}&response_type={3}'.format(
-            authorization_url, client_id,
+            authorization_url, config.client_id,
             os.path.join(request.url, 'redirect'), 'code')
     )
 
@@ -50,8 +44,8 @@ def get_access_toekn():
     authorization_code = request.args.get('code', '')
     token_url = 'https://www.douban.com/service/auth2/token'
     post_data = {
-        'client_id': client_id,
-        'client_secret': client_secret,
+        'client_id': config.client_id,
+        'client_secret': config.client_secret,
         'redirect_uri': request.base_url,
         'grant_type': 'authorization_code',
         'code': authorization_code
