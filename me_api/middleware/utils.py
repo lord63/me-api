d34d640dfa1c64da429597aae9a9c73844dffd92
lord63 <lord63.j@gmail.com>
2015-11-29 09:26:58 +0800

    Add two decorators in utils.py

--- me_api/middleware/utils.py
+++ me_api/middleware/utils.py
@@ -11,6 +11,8 @@
 
 from __future__ import absolute_import
 
+from functools import wraps
+
 from me_api.configs import Config
 
 
@@ -24,3 +26,34 @@ class MiddlewareConfig(object):
             return self.config[attr]
         if attr in self.config['data']:
             return self.config['data'][attr]
+
+
+def token_required(config):
+    """The index view for each middleware must have a token in the config.
+
+    :param config: the config for the middleware.
+    """
+    def wrapper(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            if not config.access_token:
+                return "Need access token, please authenticate your app first."
+            return func(*args, **kwargs)
+        return inner
+    return wrapper
+
+
+def reject_duplicated_auth(config):
+    """The authorization view for each middleware should reject authenticating
+    again if there's a token in the config.
+
+    :param config: the config for the middleware.
+    """
+    def wrapper(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            if config.access_token:
+                return "You've already had an access token in the config file."
+            return func(*args, **kwargs)
+        return inner
+    return wrapper
